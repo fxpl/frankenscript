6339dc58235b355fe12e2c1f0c7886393abd25a2
xFrednet <xFrednet@gmail.com>
2024-10-22 14:47:35 +0200

    Interpreter: Improve reference counting

--- src/lang/interpreter.cc
+++ src/lang/interpreter.cc
@@ -199,6 +199,18 @@ namespace verona::interpreter
       {
         std::string field{node->location().view()};
         auto v = rt::get(frame(), field);
+        if (!v)
+        {
+          if (field == "True")
+          {
+            v = rt::get_true();
+          }
+          else if (field == "False")
+          {
+            rt::get_false();
+          }
+        }
+
         rt::add_reference(frame(), v);
         stack().push_back(v);
         std::cout << "push " << v << std::endl;
@@ -308,6 +320,7 @@ namespace verona::interpreter
           result = rt::get_false();
           result_str = "false";
         }
+        rt::add_reference(frame(), result);
         stack().push_back(result);
         std::cout << "push " << result << " (" << result_str << ")"
                   << std::endl;
@@ -326,6 +339,7 @@ namespace verona::interpreter
       {
         auto v = pop("jump condition");
         auto jump = (v == rt::get_false());
+        rt::remove_reference(frame(), v);
         if (jump)
         {
           return ExecJump{node->location()};
@@ -442,7 +456,9 @@ namespace verona::interpreter
           // Setup the new frame
           for (size_t i = 0; i < func.arg_ctn; i++)
           {
-            frame->stack.push_back(parent_frame->stack.back());
+            auto value = parent_frame->stack.back();
+            frame->stack.push_back(value);
+            rt::move_reference(parent_frame->frame, frame->frame, value);
             parent_frame->stack.pop_back();
           }
         }
