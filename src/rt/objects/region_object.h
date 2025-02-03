d47d897ca6df5b03fc2e18a8420159a8cd450f3f
Max <Jarziz80@gmail.com>
2025-02-03 15:56:00 +0100

    Seemingly correct diagrams, further testing needed

--- src/rt/objects/region_object.h
+++ src/rt/objects/region_object.h
@@ -23,18 +23,24 @@ namespace rt::objects
       auto region = get_region(this);
 
       std::stringstream stream;
-      stream << this << std::endl;
-
       // Happens if an existing region has been frozen
       if (region == immutable_region)
       {
+        stream << this << std::endl;
         stream << "lrc=<frozen>" << std::endl;
         stream << "sbrc=<frozen>";
       }
+      // False if region was at some point merged with another
+      // Merged regions should not display 'lrc' nor 'sbrc' 
+      else if (this != region->bridge)
+      {
+        stream << this;
+      }
       else
       {
+        stream << this << std::endl;
         stream << "lrc=" << region->local_reference_count
-               << (region->is_lrc_dirty ? " (dirty)" : "") << std::endl;
+                << (region->is_lrc_dirty ? " (dirty)" : "") << std::endl;
         stream << "sbrc=" << region->sub_region_reference_count;
       }
       return stream.str();
