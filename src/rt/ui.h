fed4e5c5dc311e38e26c09e196e93bde76aa7741
xFrednet <xFrednet@gmail.com>
2025-03-07 17:30:23 +0100

    CLI: Allow specifying the output `.md` file via `--out`

--- src/rt/ui.h
+++ src/rt/ui.h
@@ -2,6 +2,7 @@
 
 #include "objects/visit.h"
 
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <set>
@@ -13,6 +14,8 @@ namespace rt::ui
   class UI
   {
   public:
+    virtual void set_output_file(std::string path_) = 0;
+
     virtual void output(std::vector<objects::DynObject*>&, std::string) {}
 
     virtual void highlight(std::string, std::vector<objects::DynObject*>&) {}
@@ -76,6 +79,12 @@ namespace rt::ui
 
   public:
     MermaidUI();
+
+    void set_output_file(std::string path_) override
+    {
+      assert(path_.ends_with(".md"));
+      this->path = path_;
+    }
 
     void output(
       std::vector<objects::DynObject*>& roots, std::string message) override;
